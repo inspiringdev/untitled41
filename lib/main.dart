cc7607cdee15c40af7833f3088afb516cf4531e6
Ubaid ur rehman <152322766+inspiringdev@users.noreply.github.com>
2025-05-18 20:00:26 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+// FirebaseOptions for web
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: "AIzaSyAVaO8yHXww2oZerIo2xW9piHQe2IUJXwE",
+  authDomain: "tiger-u5.firebaseapp.com",
+  projectId: "tiger-u5",
+  storageBucket: "tiger-u5.firebasestorage.app",
+  messagingSenderId: "832230723962",
+  appId: "1:832230723962:web:99c027dfe7d2234e6ec6c3",
+  measurementId: "G-E1DWH7ZLX4",
+);
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: firebaseOptions, // Provide FirebaseOptions for web
+  );
   runApp(MyApp());
 }
 
@@ -17,7 +30,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Define the PostScreen widget
+// Define PostScreen
 class PostScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
